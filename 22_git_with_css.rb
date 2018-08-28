a52e2738faff46bab50fc592f1346f38d0f1c4d4
Flynn <matt.etherington@coderacademy.edu.au>
2018-08-28 13:29:47 +1000

    Initial commit - 22_git_with_css

---
+++ 22_git_with_css.rb
@@ -0,0 +1,8 @@
+# - Make a new directory called “practice-portfolio” with a HTML file (copy some code from other projects you have that will display something) and a README.md
+# - Set up a git repository with the same name and make your first commit.
+# - Create a new branch that adds a new form feature to your html file, and a CSS file for your stylings. Checkout this branch and ‘ls’ to see the file structure. Checkout your master and do the same. Now change back to the branch and add some styles.
+# - Commit this to your current branch
+# - (you need to do this:  git push origin my-new-feature-branch)
+# - Merge your branch with the master
+# - Now you want to add a CSS feature but aren’t sure how it will look on the page. Make a new branch and create a small feature to see how this will look. 
+# - Commit the master, push, and investigate the branches. 
