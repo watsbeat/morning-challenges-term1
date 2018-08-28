24cf246c72f5f32da61efdfe6b1e40705621ed34
Flynn <matt.etherington@coderacademy.edu.au>
2018-08-28 13:28:49 +1000

    Initial commit - 15_chocolate_cake

---
+++ 15_chocolate_cake.rb
@@ -0,0 +1,1 @@
+# Write a program that prints the numbers from 1 to 100. For multiples of three print “Chocolate” instead of the number and for the multiples of five print “Cake”. For numbers which are multiples of both three and five print “ChocolateCake”.
