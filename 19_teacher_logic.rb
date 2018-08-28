01c4b9716f3395efbcc1223c9209f0bf5878883a
Flynn <matt.etherington@coderacademy.edu.au>
2018-08-28 13:29:28 +1000

    Initial commit - 19_teacher_logic

---
+++ 19_teacher_logic.rb
@@ -0,0 +1,13 @@
+# Each of five teachers - Ruegen, Sally, Matt, Gretchen, and Harrison will visit exactly one of three cities - Melbourne, Sydney, or Brisbane - for the month of April, according to the following conditions:
+# - Harrison visits a different city than Matt.
+# - Ruegen visits the same city as Gretchen.
+# - Sally visits Melbourne or else Sydney.
+# - If Matt visits Brisbane, Ruegen visits Brisbane with him.
+# - Each teacher visits one of the cities with at least one of the other four teachers. 
+
+# If Harrison visits Brisbane, which one of the following must be true?
+# a) Ruegen visits Melbourne
+# b) Sally visits Melbourne
+# c) Matt visits Melbourne
+# d) Sally visits the same city as Matt
+# e) Sally visits the same city as Gretchen
