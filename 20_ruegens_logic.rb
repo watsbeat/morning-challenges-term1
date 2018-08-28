eb8994cdfe0aa5661eb5e3205ccbbdc85b55f1d0
Flynn <matt.etherington@coderacademy.edu.au>
2018-08-28 13:29:38 +1000

    Initial commit - 20_ruegens_logic

---
+++ 20_ruegens_logic.rb
@@ -0,0 +1,14 @@
+# Ruegen has a small antiques shop in Footscray and is always looking out for items at markets to make up and stock on his shelves. Last weekend he found six containers for known down prices that he thought he’d be able to knock back up again. From the clues, can you fill in the name of each of the stall holders Ruegen brought from and both the country of origin and the description of the container?
+
+# CLUES:
+# 1. Ruegen fond the pewter jug on Erroll’s stall, which was numbered next lower than the stall with the item of Italian origin, and immediately thought of the landlord of the Rusty Bucket who likes to have these sort of things on the shelf behind his bar
+# 2. The large earthenware pitcher, which Ruegen thought he could pass on to The Old Mill Hotel as a period decoration, was not on stall 8.
+# 3. The item of Belgian origin was on Sandy’s stall, numbered one higher than the one where Ruegen found the porcelain vase, which Ruegen thought would do best in the window of his shop.
+# 4. reuse found the Dutch item on stall 6; the silver hip flask which Ruegen planned to pass straight on to auctioneers for their special silver collectors’ sale next week, it wasn’t on stall 7 or stall 8.
+# 5. Barney’s stall wasn’t stall 2 or stall 11, and the French item wasn’t on Jane’s stall.
+# 6. Ruegen found the gold jug, which wasn’t made in Britain, amongst the clutter on stall 2, and it was so cheap he knew that even if he couldn’t find a buyer, he could make a profit on the scrap value. Steve and his wife were on stall 4.
+# 7. the brass pill box, which Ruegen knew would be taken off his hands by the Mayor, had been crafted in the German city of Munich.
+# Stall-holders: Barney, Erroll, Jane, Kirk, Sandy, Steve.
+# Items: brass pill box, earthenware pitcher, gold jug, pewter jug, porcelain vase, silver hip flask.
+# Countries of origin: Belgian, British, Dutch, French, German, Italian.
+# HINT: work out what Ruegen bought from Sandy as a starting point.
