4383459527c791e58eb321bf61efbf17eea9a3e3
watsbeat <c.watsonbeaton@gmail.com>
2018-09-27 15:36:46 +1000

    edit

--- 10_ruby_fight.rb
+++ 10_ruby_fight.rb
@@ -7,3 +7,5 @@ You’ll need a few methods, consider one that keeps track of whether a player i
 You’ll then need to make a fight method that uses a loop to make the players hit one another until  the is alive method returns false for one of them. When the loop is finished (which would mean one of the players has lost), show the players output.
 
 Then create two Player objects (create a random value for each player’s health and power, make is so that the maximum health value for a player is 200, and the maximum power value is 25) and call the fight method.
+
+#
