f474d751c854e990427f894a1c71b1fee7aff30b
watsbeat <c.watsonbeaton@gmail.com>
2018-10-04 14:21:54 +1000

    solved data stype challenge

--- 01_ruby_intro.rb
+++ 01_ruby_intro.rb
@@ -9,14 +9,16 @@
 #     2. How many minutes are in a decade?
 #     3. How many seconds old are you?
 
+# To open IRB in the command line, type "irb".
+#   1. 365 * 24 =  8760
+#   2. 8760 * 60 * 10 = 5256000
+#   3. 8760 * 60 * 60 * 24 = 756864000 
 
 
 # What do you think happens when you combine the following floats and integers?
 # Is the result a float or an integer?
 # Try computing these in irb —
-#     1. 3.0 / 2
-#     2. 3 / 2.0
-#     3. 4 ** 2.0
-#     4. 4.1 % 2
-
-#
+#     1. 3.0 / 2 = 1.5 (float)
+#     2. 3 / 2.0 = 1.5 (float)
+#     3. 4 ** 2.0 = 16.0 (float)
+#     4. 4.1 % 2 = 0.09999999999999964 (modulo)
